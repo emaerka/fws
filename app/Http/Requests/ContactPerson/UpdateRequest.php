c0364395eb7163efcaba04febf24a63f757b6b15
Tódor Márk <emaerka@gmail.com>
2022-07-31 13:42:48 +0200

    unique email fix

--- app/Http/Requests/ContactPerson/UpdateRequest.php
+++ app/Http/Requests/ContactPerson/UpdateRequest.php
@@ -25,7 +25,7 @@ class UpdateRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|max:255|unique:contact_people,email,' . $this->id,
         ];
     }
 }
